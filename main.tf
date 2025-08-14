d93cd86ca0b10c67b7f72c7e45f827bedcd1a106
Dominic Laycock <dtlaycock@users.noreply.github.com>
2025-08-14 11:05:49 -0400

    removing custom backend (unintentional commit)

--- main.tf
+++ main.tf
@@ -7,13 +7,6 @@ terraform {
     }
   }
   required_version = ">= 1.0"
-  backend "remote" {
-      hostname = "strategicus.jfrog.io"
-      organization = "dominicl-terraform-state-local"
-      workspaces {
-        prefix = "dominicl-arti-install-"
-      }
-  }
 }
 
 provider "aws" {
